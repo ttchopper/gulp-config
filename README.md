b23385f8f482e3219b573da16191922c60429707
andrew <twinklechopper@gmail.com>
2016-10-17 00:31:26 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# my personal gulp-config
---
+++ gulpfile.babel.js
@@ -0,0 +1,104 @@
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import gulpStylus from 'gulp-stylus';
+import debug from 'gulp-debug';
+import sourcemaps from 'gulp-sourcemaps';
+import del from 'del';
+import browserSync from 'browser-sync';
+import { resolver } from 'stylus';
+import autoprefixer from 'gulp-autoprefixer';
+import plumber from 'gulp-plumber';
+import svgSprite from 'gulp-svg-sprite';
+import cheerio from 'gulp-cheerio';
+
+const bs = browserSync.create();
+
+gulp.task('clean', () => {
+    return del(['build/**/*', '!build/.git' ]);
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/root/index.pug')
+        .pipe(plumber())
+        .pipe(pug(
+            {
+                pretty: true,
+                baseDir: 'src'
+            }
+        ))
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('styles', () => {
+    return gulp.src('src/root/index.styl')
+
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(gulpStylus(
+            {
+                compress: false,
+                define: {
+                    url: resolver()
+                }
+            }
+        ))
+        .pipe(autoprefixer(
+            {
+                browsers: ['last 7 iOS versions']
+            }
+        ))
+        .pipe(plumber())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('assets', () => {
+    return gulp.src('src/**/*.{png,jpg,JPG}')
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('sprite', () => {
+    return gulp.src('src/**/*.svg')
+        .pipe(cheerio(($, file, done) => {
+            // Removing fill attribute
+            $('[fill]').removeAttr('fill');
+            done();
+        }))
+        .pipe(svgSprite(
+            {
+                mode: {
+                    dimensionAttributes: true,
+                    symbol: {
+                        sprite: __dirname + '/img/sprite.css.svg'
+                    }
+                }
+            }
+        ))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', gulp.series(
+    'clean',
+    gulp.parallel('styles', 'html', 'assets', 'sprite'))
+);
+
+gulp.task('watch', () => {
+    gulp.watch('src/**/*.styl', gulp.series('styles'));
+    gulp.watch('src/**/*.pug', gulp.series('html'));
+    gulp.watch('src/**/*.{png,jpg}', gulp.series('assets'));
+    gulp.watch('src/**/*.svg', gulp.series('sprite'));
+})
+
+
+
+gulp.task('serve', () => {
+    bs.init({
+        server: 'build'
+    });
+
+    bs.watch('build/**/*.*').on('change', bs.reload);
+});
+
+gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
